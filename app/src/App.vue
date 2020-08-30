7f09b7a08dee71dface776f76cd9ae9120cb9882
BenjaKemp <BenjoKemp@gmail.com>
2020-08-30 11:56:42 +0200

    fun with vuetify

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <navigation-drawer></navigation-drawer>
+    <v-app-bar
+      app
+      dark
+      elevate-on-scroll 
+    >
+    <!-- <v-app-bar-nav-icon></v-app-bar-nav-icon> -->
+
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="./assets/The-Wonk-forweb-1200x442.jpg"
+          transition="scale-transition"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld';
+import NavigationDrawer from './components/navigation/NavigationDrawer';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  components: {
+    HelloWorld,
+    NavigationDrawer
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
--- app/src/components/partials/button.vue
+++ app/src/components/partials/button.vue
@@ -84,21 +84,23 @@ export default {
         },
 
         colorVariants() {
+            console.log('this variant  inside   ',this.variant)
+            console.log('this variantType  inside   ',this.variantType)
             switch (this.variant) {
                 case "primary":
                     switch (this.variantType) {
                         case "normal":
                             switch (this.disableButton) {
                                 case true:
-                                    return "border-blue-300 bg-blue-300 text-white";
+                                    return "primary-blue-300 bg-blue-300 text-white";
                                     break;
                                 default:
                                     break;
                             }
-                            return "border-blue-600 bg-blue-600 hover:bg-blue-700 hover:border-blue-700 text-white";
+                            return "primary-blue bg-blue-600 hover:bg-blue-700 hover:border-blue-700 text-white";
                             break;
                         case "outline":
-                            return "border-gray-200 text-blue-500 hover:text-blue-700";
+                            return "primary-blue-outline text-blue-500 hover:text-blue-700";
                             break;
                         default:
                             break;
@@ -127,8 +129,10 @@ export default {
                 case "warning":
                     switch (this.variantType) {
                         case "normal":
-                            return "border-orange-600 bg-orange-600 hover:bg-orange-700 hover:border-orange-700 text-white";
+                            return "warning-orange";
                             break;
+                        case "outline":
+                            return "warning-orange-outline";
                         default:
                             break;
                     }
@@ -178,6 +182,55 @@ export default {
                     break;
             }
         }
+    },
+    updated () {
+        console.log('this      ',this)
+        console.log('this.variantType     ',this.variantType)
+        console.log('this.variant     ',this.variant)
+
     }
 };
 </script>
+
+<style>
+.warning-orange {
+    border: orange solid 5px;
+    border-radius: 5px;
+}
+.warning-orange-outline {
+    background-color: white;
+    color: white;
+    border: 2px solid orange; /* Green */
+}
+.danger-red {
+    background-color: red;
+    color: white;
+    border: 2px solid red; /* Green */
+}
+.danger-red-outline {
+    background-color: white;
+    color: black;
+    border: 2px solid red; /* Green */
+}
+.primary-blue {
+    background-color: blue;
+    color: white;
+    border: 2px solid blue; /* Green */
+}
+.primary-blue-outline {
+    background-color: white;
+    color: black;
+    border: 2px solid blue; /* Green */
+}
+.success-green {
+    background-color: green;
+    color: white;
+    border: 2px solid green; /* Green */
+}
+.success-green-outline {
+    background-color: white;
+    color: black;
+    border: 2px solid green; /* Green */
+}
+
+</style>
