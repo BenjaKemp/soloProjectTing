c3fb86b54e6280006266058734d34fb5ebb74d16
Ben Kemp <ben@Cartss-MacBook-Pro.local>
2020-09-22 23:37:54 +0200

    routes locked

--- server/router.js
+++ server/router.js
@@ -1,20 +1,13 @@
 const Authentication = require('./controllers/authentication');
-// const passportService = require('./services/passport')
-const passport = require('passport');
+const passport = require('./services/passport')
 
 const path = require('path')
-const index = path.join('../app/dist/', 'index.html');
-const requireAuth = passport.authenticate('jwt', {
-  session: false
-});
-const requireSignin = passport.authenticate('local', {
-  session: false
-});
 
 module.exports = (app) => {
+  app.use('*', passport.requireAuth);
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
   })
-  app.post('/signin', requireSignin, Authentication.signin);
+  app.post('/signin', Authentication.signin);
   app.post('/signup', Authentication.signup);
 }
