134c27f0466df45b10de007aeafecbdf43c56194
Ben Kemp <ben@Cartss-MacBook-Pro.local>
2020-09-23 23:14:55 +0200

    logout finished

--- server/router.js
+++ server/router.js
@@ -2,10 +2,10 @@ const Authentication = require('./controllers/authentication');
 const passport = require('./services/passport')
 
 module.exports = (app) => {
-  app.use('*', passport.requireAuth);
+  app.post('/signin', Authentication.signin);
+  app.post('/signup', Authentication.signup);
+  app.use('/graphql/', passport.requireAuth);
   app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html')
   })
-  app.post('/signin', Authentication.signin);
-  app.post('/signup', Authentication.signup);
 }
