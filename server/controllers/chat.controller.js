abfaec9afb90af9adb0d46e686aa95b1c0758ec9
Ben Kemp <ben@Cartss-MacBook-Pro.local>
2020-10-11 08:06:47 +0200

    editing

---
+++ server/controllers/chat.controller.js
@@ -0,0 +1,41 @@
+
+
+class ChatClass {
+    constructor(socket){
+        this.socket = socket
+    }
+    sendPost(room, msg) {
+        this.socket.emit('message', {
+            text: msg,
+            room
+        })
+    }
+    changeRoom (room) {
+        this.socket.emit('join', {
+            newRoom: room
+        })
+    }
+    processCommand(command) {
+        const words = command.split(' ')
+        const parsedCmd = words[0].substring(1, words[0].length).toLowerCase()
+        let msg = false
+
+        switch (parsedCmd) {
+            case 'join':
+                words.shift()
+                const room = words.join(' ')
+                this.changeRoom(room)
+                break
+            case 'nick':
+                words.shift()
+                const name = words.join(' ')
+                this.socket.emit('nameAttempt', name)
+                break
+            default:
+                msg = 'Unrecognized command.'
+                break
+        }
+        return msg
+    }
+}
+module.exports = ChatClass
--- server/index.js
+++ server/index.js
@@ -1,39 +1,25 @@
 const express = require('express');
-
 const cors = require('cors')
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
-
 const chatServer = require('./sockets')
-
-
-
-
 // const config = require('../app/webpack.config.js');
 // const index = require('../app/dist/index.html');
-
 // const compiler = webpack(config);
 const app = express();
 const http = require('http').Server(app)
 chatServer.listen(http)
 // const io = require('socket.io')(http);
 const router = require('./router');
-
 app.use(cors())
 app.use(morgan('combined'));
 app.use(bodyParser.json({
   type: '*/*'
 }));
-
 // Tell express to use the webpack-dev-middleware and use the webpack.config.js
 // configuration file as a base.
 // app.use(webpackDevMiddleware(compiler, {
 //   publicPath: config.output.publicPath,
 // }));
-
 router(app);
-
-
-
 module.exports = app;
