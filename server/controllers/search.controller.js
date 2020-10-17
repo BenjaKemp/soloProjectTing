6d9d1766188564420391fa84cf7760dc226198ee
Ben Kemp <benjokemp@gmail.com>
2020-10-17 14:59:43 +0200

    working reed search

--- server/controllers/search.controller.js
+++ server/controllers/search.controller.js
@@ -4,8 +4,8 @@ const searchJooble = require('../services/websearches/jooble.search')
 class SearchClass {
 static async metaSearch(input){
     const res = await search(input)
-    const joobleRes = await searchJooble(input)
-    console.log('this is joobl        ', joobleRes)
+    // const joobleRes = await searchJooble(input)
+    // console.log('this is joobl        ', joobleRes)
     return res
 }
 }
